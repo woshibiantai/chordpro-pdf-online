e8b3d3afea5411b803a3845dc096290d8af9547e
Ruth Wong <ruthwongny@gmail.com>
2024-07-21 19:31:19 +0800

    Resize font based on scree size

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,3 +8,33 @@
 import { defineAsyncComponent } from 'vue';
 const ChordChart = defineAsyncComponent(() => import('../components/ChordChart.vue'));
 </script>
+
+<style scoped>
+main {
+  font-size: 16px;
+
+  @media screen and (max-width: 1280px) {
+    font-size: 14px;
+  }
+
+  @media screen and (max-width: 1024px) {
+    font-size: 13px;
+  }
+
+  @media screen and (max-width: 875px) {
+    font-size: 12px;
+  }
+
+  @media screen and (max-width: 768px) {
+    font-size: 11px;
+  }
+
+  @media screen and (max-width: 576px) {
+    font-size: 10px;
+  }
+
+  @media screen and (max-width: 475px) {
+    font-size: 9px;
+  }
+}
+</style>
