027c5feddc66f46c4b8d1a72cc5041bca9425b6f
Ruth Wong <ruthwongny@gmail.com>
2024-09-08 21:30:29 +0800

    Fetch chordpro file from Google drive

--- src/App.vue
+++ src/App.vue
@@ -12,15 +12,30 @@
 import { provide, ref } from 'vue';
 import { RouterLink, RouterView } from 'vue-router'
 
+const searchParams = new URLSearchParams(location.search);
+const googleDriveFileId = searchParams.get('googleDriveFileId');
 const chordProInput = ref('');
 provide('chordProInput', chordProInput);
 
-import('@/assets/sample-chart.js')
-  .then(({ sampleChordProChart }) => {
-    if (!chordProInput.value) {
-      chordProInput.value = sampleChordProChart;
+if (googleDriveFileId) {
+  fetch(`https://www.googleapis.com/drive/v3/files/${googleDriveFileId}?alt=media`, {
+    method: 'GET',
+    headers: {
+      'X-goog-api-key': import.meta.env.VITE_GOOGLE_API_KEY,
     }
-  });
+  })
+    .then(response => response.text())
+    .then(text => {
+      chordProInput.value = text;
+    });
+} else {
+  import('@/assets/sample-chart.js')
+    .then(({ sampleChordProChart }) => {
+      if (!chordProInput.value) {
+        chordProInput.value = sampleChordProChart;
+      }
+    });
+}
 </script>
 
 <style scoped>
